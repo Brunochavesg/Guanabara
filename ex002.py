425f5e202f7f9fe30102a8bdb2a2eb3e8bd6c4ab
Bruno Chaves <brunochavesg@gmail.com>
2020-04-01 11:19:10 -0300

    Novos

---
+++ ex002.py
@@ -0,0 +1,7 @@
+nome = input("Qual o seu nome?: ")
+print ("Seja bem vindo {}".format(nome)) #pode usar assim tbm ou print ("Seja bem vindo", nome)
+
+
+import os
+os.system("pause")
+print (a.isspace())
---
+++ ex003.py
@@ -0,0 +1,4 @@
+n1 = int(input('Digite o primeiro numero: '))
+n2 = int(input('Digite o segundo numero: '))
+s = n1+n2
+print (f"A soma entre os numeros {n1} + {n2} é = {s}!")
