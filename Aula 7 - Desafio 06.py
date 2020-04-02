ba95819ad4a6abdc89e92f1b13ac4d5c4d9f91d5
Bruno Chaves <brunochavesg@gmail.com>
2020-04-02 16:22:37 -0300

    Atualização de Valores Finais

--- Aula 7 - Desafio 06.py
+++ Aula 7 - Desafio 06.py
@@ -1,4 +1,10 @@
-n1 = float(input ("Quanto de dinheiro você tem na carteira?: "))
-dolar = float(3.27)
+n1 = float(input ("Quanto de dinheiro você tem na carteira? R$"))
+tipo = input ("Você quer convertar para dolar ou euro?")
+dolar = float(5.27)
+if tipo == "DOLAR":
+    print (f"Você tem R${n1} Reais e pode comprar US${n1/dolar:.2f} Dolar")
+elif tipo == "EURO":
+    print (f"Você tem R${n1} Reais e pode comprar EU${n1/dolar:.2f} Euro")
+else:
+    print ("Moeda desconhecida, digite dolar ou.")
 
-print (f"Você tem R${n1} Reais e pode comprar U${n1/dolar:.2f} Dolar")
