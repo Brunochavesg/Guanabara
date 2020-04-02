e12b6bf7c9fafd74048ac675b505b7dd958844a6
Bruno Chaves <brunochavesg@gmail.com>
2020-04-02 14:03:11 -0300

    Resultado Final

--- Aula 7 - Desafio 03.py
+++ Aula 7 - Desafio 03.py
@@ -1,6 +1,6 @@
-n1 = int(input("Primeira nota"))
-n2 = int(input("Segunda nota"))
-s = ((n1 + n2) / 2)
+n1 = float(input("Primeira nota: "))
+n2 = float(input("Segunda nota: "))
+s = (n1 + n2) / 2
 
-print (s)
+print(f"A Media da nota é {s:.1f}")
 
