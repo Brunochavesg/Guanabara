43f429764f4085e24a2c91d80bb6af4eacd6fe86
Bruno Chaves <brunochavesg@gmail.com>
2020-04-02 13:40:32 -0300

    Alteração no resultado da raiz quadrada

--- Aula 7 - Desafio 02.py
+++ Aula 7 - Desafio 02.py
@@ -1,5 +1,3 @@
 n1 = int(input("Digite um numero: "))
-print (f'O numero digitado foi: {n1} o dobro de {n1} é {n1*2} o triplo de {n1} é {n1*3} e a raiz quadrada de {n1} é {n1**2}')
+print (f'O numero digitado foi: {n1} o dobro de {n1} é {n1*2} o triplo de {n1} é {n1*3} e a raiz quadrada de {n1} é {n1**(1/2):.2f}')
 
-import os
-os.system ("pause")
