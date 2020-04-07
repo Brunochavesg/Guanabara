a80bd11dcb01bc51728050f309384d3692c7f5fd
Bruno Chaves <brunochavesg@gmail.com>
2020-04-07 01:34:15 -0300

    Tirei o Import os

--- Aula 4 - Desafio 01.py
+++ Aula 4 - Desafio 01.py
@@ -2,5 +2,3 @@
 nome = input("Qual o seu nome? ")
 print ("Ola " +nome,", Seja bem vindo!")
 
-import os
-os.system("pause")
--- Aula 7 - Desafio 07.py
+++ Aula 7 - Desafio 07.py
@@ -1,4 +1,3 @@
 largura = float(input("Qual a largura?: "))
 altura = float(input("Qual a altura?: "))
 print (f"Sua área é de {largura * altura:.2f}m² e precisara de {largura * altura / 2:.1f} litros de tinta" )
-.
