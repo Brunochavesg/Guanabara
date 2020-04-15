e2a6d17cd5955d64f92743c4f22ba62a5ac77e32
Bruno Chaves <brunochavesg@gmail.com>
2020-04-14 21:31:55 -0300

    complemento

--- exFinal01_15.py
+++ exFinal01_15.py
@@ -11,3 +11,8 @@ print (f"Você nasceu dia {dia} de {mes} do ano de {ano}.")
 sal_ini = float(input("Qual seu salário? R$"))
 sal_mes = float(input("Quantos Meses você trabalho com esse Salário?"))
 print (f"Seu sálario inicial foi R${sal_ini:.2f} e somando todos os meses você ganhou R${sal_ini * sal_mes:.2f}")
+real = float(input("Digite um valor em Real: R$"))
+print (f"Com R${real} reais voce consegue comprar US${real / 4.90:.2f}")
+
+
+
